940b64e452d2bd2f70b1a8299e395a3eb6d5ef49
Kailee Gray <kaileeagray@gmail.com>
2019-05-03 16:45:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ function getFirstSelector(selector) {
 // (# is used for IDs in selectors — but you knew that because you read the
 // docs, right? :) ). (Note that in index.html #nested and .target just happen
 // to be divs. This method should work with arbitrary elements.)
+function nestedTarget() {
+  
+}
 
 // Define a function increaseRankBy(n) that increases the ranks in all of the
 // .ranked-lists by n. (You might need to make use of parseInt()
