021b355220d6fa3939e0fc9edcf75a3362ef76a6
Kailee Gray <kaileeagray@gmail.com>
2019-05-03 16:49:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nestedTarget() {
 // Define a function increaseRankBy(n) that increases the ranks in all of the
 // .ranked-lists by n. (You might need to make use of parseInt()
 function increaseRankBy(n) {
-  var document.querySelectorAll('ul.ranked-list');
+  var rankedLists = document.querySelectorAll('ul.ranked-list');
 }
 
 // Define a function deepestChild() that pulls out the most deeply nested
