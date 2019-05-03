62c1511cd9c944ad0ef8ffd5550b83f032e8bf2a
Kailee Gray <kaileeagray@gmail.com>
2019-05-03 16:43:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Define a function getFirstSelector(selector), which accepts a selector and
 // returns the first element that matches.
-
+function getFirstSelector(selector) {
+  
+}
 
 
 // Define a function nestedTarget() that pulls a .target out of #nested
