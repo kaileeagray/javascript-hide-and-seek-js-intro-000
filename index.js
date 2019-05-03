b426d9b7f453bc895d4b5e48f7376498b922bdcf
Kailee Gray <kaileeagray@gmail.com>
2019-05-03 17:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ function increaseRankBy(n) {
 function deepestChild() {
   var current = document.querySelector('#grand-node').firstElementChild;
   var next = current.firstElementChild;
-  
+  while (current)
 
 }
