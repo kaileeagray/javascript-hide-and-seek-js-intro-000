0e6d6e77d1226e17bc352f06602c92fc22e72df1
Kailee Gray <kaileeagray@gmail.com>
2019-05-03 16:42:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+// Define a function getFirstSelector(selector), which accepts a selector and 
+// returns the first element that matches.
 
+// Define a function nestedTarget() that pulls a .target out of #nested 
+// (# is used for IDs in selectors — but you knew that because you read the 
+// docs, right? :) ). (Note that in index.html #nested and .target just happen 
+// to be divs. This method should work with arbitrary elements.)
+
+// Define a function increaseRankBy(n) that increases the ranks in all of the 
+// .ranked-lists by n. (You might need to make use of parseInt()
+
+
+// Define a function deepestChild() that pulls out the most deeply nested 
+// child element from div#grand-node. (Remember, you can iterate over elements 
+// and call querySelector() and querySelectorAll() on them. This is challenging 
+// to implement correctly, but not beyond your ability!)
+// HINT 1: Your solution for deepestChild() does not need to be totally generic; 
+// we don't expect it to work in every case. For example, we know that 
+// div#grand-node has only one node at each level — for this lab, you can s
+// olve for that case, and not worry about a case where there are sibling nodes.
